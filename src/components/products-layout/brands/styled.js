7c6b0d44f6c84c8e299f29f3a4fec6058204ba82
V-Babayan <vladislavbabayan@gmail.com>
2022-04-24 17:05:32 +0400

    refactor: group products page layout components

---
+++ src/components/products-layout/brands/styled.js
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+import smallCheckbird from "assets/icons/smallCheckbird.svg";
+
+const HeadingBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 14px;
+`;
+
+const Title = styled.h5`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1.5;
+
+  color: ${({ theme }) => theme.color.blue};
+`;
+
+const ResetButton = styled.button`
+  font-size: 15px;
+  line-height: 1.5;
+
+  color: #595959;
+`;
+
+const BrandList = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 10px;
+  @media screen and (${({ theme }) => theme.media("mobileL")}) {
+    display: block;
+  }
+`;
+
+const Brand = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 12px;
+`;
+
+const BrandCheckbox = styled.input`
+  appearance: none;
+  width: 29px;
+  height: 29px;
+
+  background: #b3d4e5;
+  border: 1px solid #b3d4e5;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background, border 0.2s linear;
+
+  &:checked {
+    background: #3f3f3f;
+    border: 1px solid #a5a5a5;
+    border-radius: 8px;
+
+    ::after {
+      content: url(${smallCheckbird});
+    }
+  }
+`;
+const BrandInfo = styled.div`
+  flex: 1 1 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export { HeadingBlock, Title, ResetButton, BrandList, Brand, BrandCheckbox, BrandInfo };
