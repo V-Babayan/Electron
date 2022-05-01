d336f55c3d1390727cab47bb26373c40d0d3882c
V-Babayan <vladislavbabayan@gmail.com>
2022-05-01 18:47:03 +0400

    feat: Add new slider logic on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,54 @@
 import React, { useMemo } from "react";
+import { useMedia } from "hooks";
 import { useSelector } from "react-redux";
 
-import { Slider } from "components/slider";
-import List from "components/list";
 import { selectRelatedProducts } from "store";
+
+import { Slider } from "components/slider";
+import { Heading } from "components/core-ui";
+import { List } from "components/list";
+
+import { sliceProductsToPages } from "helpers";
+import { Page } from "components/slider/children/";
 
 const Home = () => {
   const products = useSelector(selectRelatedProducts);
+  const isMobile = useMedia("mobile");
+  const isTablet = useMedia("tablet");
+  const isNotebook = useMedia("notebook");
 
   const sliderProducts = products.length ? [...products].slice(0, 4) : [{ id: Date.now() }];
 
-  const listProducts = useMemo(() => products.slice(0, 12), [products]);
+  const listProducts = useMemo(() => {
+    if (isNotebook) return sliceProductsToPages(products, 8);
+
+    if (isTablet) return sliceProductsToPages(products, 6);
+
+    if (isMobile) return sliceProductsToPages(products, 4);
+
+    return sliceProductsToPages(products, 1);
+  }, [products, isMobile, isTablet, isNotebook]);
 
   return (
     <>
-      <Slider products={sliderProducts} />
-      <List
-        products={listProducts}
-        title={"Popular products"}
-      />
+      <Slider>
+        {sliderProducts.map((product) => (
+          <Page
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </Slider>
+
+      <Heading>Popular products</Heading>
+      <Slider list>
+        {listProducts.map(({ id, products }) => (
+          <List
+            key={id}
+            products={products}
+          />
+        ))}
+      </Slider>
     </>
   );
 };
