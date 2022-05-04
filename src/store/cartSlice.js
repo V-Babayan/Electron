45d8c793c1b21602879cb33bdb4e25359e27a6bd
V-Babayan <vladislavbabayan@gmail.com>
2022-05-04 21:18:39 +0400

    wip: Change cart addProduct action logic

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { indexOfCart } from "helpers";
 
 const cartSlice = createSlice({
   name: "cartSlice",
@@ -7,14 +8,36 @@ const cartSlice = createSlice({
     setCart(state, action) {
       state.cart = action.payload;
     },
+
     emptyCart(state) {
       state.cart = [];
     },
+
     deleteElement(state, action) {
       state.cart = state.cart.filter((elem) => elem.id !== action.payload);
     },
-    addProduct(state, { payload }) {
-      state.cart.push(payload);
+
+    addProduct({ cart }, { payload }) {
+      // const index = indexOfCart(cart, payload.product.id);
+
+      // if (typeof index === "number") cart[index].count += payload.quanity;
+      // else cart.push({ product: payload.product, count: payload.quanity, id: cart.length + 1 });
+
+      let haveInCart = false;
+
+      cart.forEach((cartItem) => {
+        if (cartItem.product.id === payload.product.id) {
+          cartItem.count += payload.quanity;
+          haveInCart = true;
+        }
+      });
+
+      if (!haveInCart)
+        cart.push({
+          product: payload.product,
+          count: payload.quanity,
+          id: cart.length + 1,
+        });
     },
   },
 });
