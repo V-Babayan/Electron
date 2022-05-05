edfd376b36a1077789df2e80dae59347b9d4dafb
V-Babayan <vladislavbabayan@gmail.com>
2022-05-05 18:28:35 +0400

    refactor: Remove setCart action.

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 export { asyncGetProducts } from "./actions";
 
-export { setCart, emptyCart, deleteElement, addProduct } from "./cart/cartSlice";
+export { emptyCart, deleteElement, addProduct } from "./cart/cartSlice";
 export { setProducts } from "./productsSlice";
 
 export { selectRelatedProducts, selectCategories, selectCart, selectProducts } from "./selectors";
