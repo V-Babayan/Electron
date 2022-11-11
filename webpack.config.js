dd377010c49baf621239b9be566150968865ec6f
Vladislav Bababayan <vladislavbabayan@gmail.com>
2022-11-11 21:38:10 +0400

    chore: add webpack config and typescript config

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = [
+  {
+    entry: "./src/index.tsx",
+    mode: "development",
+    target: "web",
+    output: {
+      path: path.resolve(__dirname, "build/"),
+      filename: "bundle.js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+    resolve: {
+      extensions: [".js", ".ts", ".tsx", "jsx"],
+    },
+    module: {
+      rules: [
+        { test: /\.(ts|tsx)$/, use: "ts-loader", exclude: /node_modules/ },
+        {
+          test: /\.(js|jsx)$/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|svg|ttf|woff2)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+  },
+];
