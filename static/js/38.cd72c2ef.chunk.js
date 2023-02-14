c0e8ccdbc443d822ab20cf0b0f81c73befe11485
m-avagyan <martikavagyan1@gmail.com>
2023-02-14 17:02:13 +0400

    Updates

---
+++ static/js/38.cd72c2ef.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstore=self.webpackChunkstore||[]).push([[38],{4038:function(e,n,r){r.r(n);r(2791);var s=r(184);n.default=function(){return(0,s.jsx)("div",{children:"Login Page"})}}}]);
+//# sourceMappingURL=38.cd72c2ef.chunk.js.map
