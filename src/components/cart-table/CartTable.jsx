31126fa2002508801e243ddce8d749cf1c18253d
V-Babayan <vladislavbabayan@gmail.com>
2023-02-14 13:36:40 +0400

    feat: add CartTable component

---
+++ src/components/cart-table/CartTable.jsx
@@ -0,0 +1,72 @@
+import React, { useCallback } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { emptyCart } from "../../redux/cartClicer";
+
+import { HOME_PAGE } from "../../helpers/consts";
+import { deleteCartItem } from "../../http";
+
+import { ButtonsContainer, STable, SThead } from "./CartTable.styled";
+import CartItem from "../cart-item/CartItem";
+import Button from "../core-ui/button/Button";
+
+const CartTable = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+
+  const navigateHandler = useCallback(() => {
+    navigate(HOME_PAGE);
+  }, [navigate]);
+
+  const emptyCartHandler = useCallback(async () => {
+    for (let elem of cart) {
+      await deleteCartItem(elem.id);
+    }
+
+    dispatch(emptyCart());
+  }, [dispatch, cart]);
+
+  return (
+    <div>
+      <STable>
+        <SThead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+            <th />
+          </tr>
+        </SThead>
+        <tbody>
+          {cart.map(({ product, count, id }) => (
+            <CartItem
+              key={id}
+              item={product}
+              count={count}
+              id={id}
+            />
+          ))}
+        </tbody>
+      </STable>
+      <ButtonsContainer>
+        <Button onClick={navigateHandler}>Continue shopping</Button>
+        <Button
+          outlined
+          color={"grey"}>
+          Update cart
+        </Button>
+        <Button
+          outlined
+          color='red'
+          onClick={emptyCartHandler}>
+          Clear cart
+        </Button>
+      </ButtonsContainer>
+    </div>
+  );
+};
+
+export default CartTable;
---
+++ src/components/cart-table/CartTable.styled.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const STable = styled.table`
+  margin-bottom: 56px;
+  width: 100%;
+`;
+export const SThead = styled.thead`
+  position: relative;
+  z-index: 1;
+
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 1.5;
+
+  color: #232323;
+
+  th:first-child {
+    width: 40%;
+    padding: 10px 0 15px 34px;
+    text-align: left;
+    margin-bottom: 25px;
+  }
+
+  &::before {
+    content: "";
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    background: #e2f4ff;
+  }
+`;
+export const ButtonsContainer = styled.div`
+  display: flex;
+  gap: 65px;
+  button {
+    border-radius: 35px;
+
+    font-weight: 600;
+    font-size: 22px;
+    line-height: 1.5;
+  }
+`;
