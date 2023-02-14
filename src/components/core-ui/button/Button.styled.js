de8da9708d8374399563625d3156c0706735cfd8
V-Babayan <vladislavbabayan@gmail.com>
2023-02-14 13:28:17 +0400

    feat: add button componenet

---
+++ src/components/core-ui/button/Button.styled.js
@@ -0,0 +1,39 @@
+import styled, { css } from "styled-components";
+
+export const SButton = styled.button`
+  display: inline-block;
+  padding: ${({ theme, large }) =>
+    theme.ratio.smallPhone(10, 8) +
+    " " +
+    (large ? theme.ratio.smallPhone(10, 52) : theme.ratio.smallPhone(10, 26))};
+  border-radius: ${({ large }) => (large ? "32px" : "20px")};
+
+  font-weight: 600;
+  font-size: inherit;
+  line-height: 1.5;
+  white-space: nowrap;
+
+  ${({ outlined }) =>
+    outlined
+      ? css`
+          background: transparent;
+          color: ${({ theme, color }) => theme.color[color]};
+          transition: color, background 0.2s ease;
+          border: 1px solid ${({ theme, color }) => theme.color[color]};
+          &:hover {
+            color: #ffffff;
+            background: ${({ theme, color }) => theme.color[color]};
+          }
+        `
+      : css`
+          transition: color, background 0.2s linear;
+          background: #eda415;
+          color: #ffffff;
+          border: 1px solid #eda415;
+
+          &:hover {
+            background: transparent;
+            color: #eda415;
+          }
+        `}
+`;
