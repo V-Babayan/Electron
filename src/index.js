8a8942d197c04f19d3764e2f85ac3a71a3df4d01
V-Babayan <vladislavbabayan@gmail.com>
2023-02-15 16:12:17 +0400

    feat: add hash routing for github-pages correct routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { Provider } from "react-redux";
 
@@ -10,12 +10,14 @@ import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
+  // <Router>
+  <HashRouter>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <App />
       </ThemeProvider>
     </Provider>
-  </Router>
+  </HashRouter>
+  // </Router>
 );
