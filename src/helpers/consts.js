f4e32ab5cf79c606708a965dddb0694e493ee173
V-Babayan <vladislavbabayan@gmail.com>
2023-02-22 12:31:10 +0400

    feat: add const base url and refactore requests

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -6,15 +6,4 @@ export const CART_PAGE = "/cart";
 export const PRODUCTS_PAGE = "/products";
 export const PRODUCT_PAGE = "/products/:id";
 
-export const Categories = [
-  { id: 1, name: "Smartphones" },
-  { id: 2, name: "Notebooks" },
-  { id: 3, name: "Headphones" },
-];
-export const Brands = [
-  { id: 1, name: "Apple" },
-  { id: 2, name: "Samsung" },
-  { id: 3, name: "Xiaomi" },
-  { id: 4, name: "Huawei" },
-  { id: 5, name: "Meizu" },
-];
+export const BASE_URL = "https://63d254dc1780fd6ab9c1f850.mockapi.io/";
--- src/http/index.js
+++ src/http/index.js
@@ -1,35 +1,33 @@
 import { setCart } from "../redux/cartClicer";
 import { setProducts } from "../redux/productsClicer";
+import { BASE_URL } from "../helpers/consts";
 
-export const fetchDevices = () => async (dispatch) => {
-  const res = await fetch("https://63d254dc1780fd6ab9c1f850.mockapi.io/products");
-  const json = await res.json();
-  dispatch(setProducts(json));
+export const fetchDevices = () => (dispatch) => {
+  fetch(`${BASE_URL}products`)
+    .then((res) => res.json())
+    .then((json) => dispatch(setProducts(json)));
 };
 
-export const fetchDevice = async (id) =>
-  await fetch(`https://63d254dc1780fd6ab9c1f850.mockapi.io/products/${id}`).then((res) =>
-    res.json()
-  );
+export const fetchDevice = (id) => fetch(`${BASE_URL}products/${id}`).then((res) => res.json());
 
 export const fetchCart = () => {
-  return async (dispatch) => {
-    const res = await fetch("https://63d254dc1780fd6ab9c1f850.mockapi.io/cart");
-    const json = await res.json();
-    dispatch(setCart(json));
+  return (dispatch) => {
+    fetch(`${BASE_URL}cart`)
+      .then((res) => res.json())
+      .then((json) => dispatch(setCart(json)));
   };
 };
 
-export const deleteCartItem = (deleteId) =>
-  fetch(`https://63d254dc1780fd6ab9c1f850.mockapi.io/cart/${deleteId}`, {
+export const deleteCartItem = async (deleteId) =>
+  await fetch(`${BASE_URL}cart/${deleteId}`, {
     method: "DELETE",
   });
 
-export const addItemToCart = async (item, count) =>
-  await fetch("https://63d254dc1780fd6ab9c1f850.mockapi.io/cart", {
+export const addItemToCart = (item) =>
+  fetch(`${BASE_URL}cart`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
-    body: JSON.stringify({ product: item, count }),
+    body: JSON.stringify(item),
   });
