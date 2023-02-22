696ece52f7260b5a810072e43f6009c81bbb9825
V-Babayan <vladislavbabayan@gmail.com>
2023-02-22 18:21:12 +0400

    refactor: slice Cart page styles

---
+++ src/pages/cart/Cart.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import CartTable from "../../components/cart-table/CartTable";
+import CartTotal from "../../components/cart-total/CartTotal";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { HOME_PAGE } from "../../helpers/consts";
+import { Container, EmptyBlock } from "./Cart.styled";
+
+const Cart = () => {
+  const cart = useSelector((state) => state.cart.cart);
+
+  return cart.length ? (
+    <Container>
+      <CartTable />
+
+      <CartTotal />
+    </Container>
+  ) : (
+    <EmptyBlock>
+      Your cart is empty. <Link to={HOME_PAGE}>Go home form shoping</Link>
+    </EmptyBlock>
+  );
+};
+
+export default Cart;
