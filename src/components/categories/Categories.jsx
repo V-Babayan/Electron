fafc234412a64269147e059d0eaa6f5acc27e190
V-Babayan <vladislavbabayan@gmail.com>
2023-02-22 12:40:30 +0400

    change: change categories components for new filter in categories

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,31 +1,21 @@
 import React from "react";
-import { Category, HeadingBlock } from "./Categories.styled";
+import { Container, SInput, SLabel } from "./Categories.styled";
 
-const Categories = ({ filter = [], filterTitle, changeHandler, resetHandler, brands }) => {
+const Categories = ({ categories = [], currentCategories, changeHandler }) => {
   return (
-    <div>
-      <HeadingBlock>
-        <h5>{filterTitle}</h5>
-        <button onClick={resetHandler}>reset</button>
-      </HeadingBlock>
-      <ul>
-        {filter.map(({ name, count }) => (
-          <Category key={name}>
-            <input
-              type='checkbox'
-              value={name}
-              onChange={changeHandler}
-              checked={brands.includes(name)}
-              id={name}
-            />
-            <div>
-              <label htmlFor={name}>{name}</label>
-              <span>{count}</span>
-            </div>
-          </Category>
-        ))}
-      </ul>
-    </div>
+    <Container>
+      {categories.map((category) => (
+        <div key={category}>
+          <SInput
+            type='checkbox'
+            id={category}
+            checked={currentCategories.includes(category)}
+            onChange={changeHandler}
+          />
+          <SLabel htmlFor={category}>{category}</SLabel>
+        </div>
+      ))}
+    </Container>
   );
 };
 
