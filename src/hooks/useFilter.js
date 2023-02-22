b400559f05489c7572fa901e1dbf398a93631e01
V-Babayan <vladislavbabayan@gmail.com>
2023-02-22 12:34:33 +0400

    feat: add new hook for products filtering and brands count calculating

---
+++ src/hooks/useFilter.js
@@ -0,0 +1,27 @@
+import { useMemo } from "react";
+import { getFilter } from "../helpers/filters";
+
+export const useFilter = (products, { query, brands, categories }) => {
+  const filteredProducts = useMemo(() => {
+    const filterByQuery =
+      query.length > 0
+        ? products.filter(({ title }) => title.toLowerCase().includes(query.toLowerCase()))
+        : products;
+
+    const filterByCategories =
+      categories.length > 0
+        ? filterByQuery.filter(({ category }) => categories.includes(category))
+        : filterByQuery;
+
+    const brandsFilters = getFilter(filterByCategories, "brand");
+
+    const filterByBrands =
+      brands.length > 0
+        ? filterByCategories.filter(({ brand }) => brands.includes(brand))
+        : filterByCategories;
+
+    return [filterByBrands, brandsFilters];
+  }, [query, brands, categories, products]);
+
+  return filteredProducts;
+};
