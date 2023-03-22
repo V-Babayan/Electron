67e7b53815ea149c7cd71d800b29d1ffc10b91d8
V-Babayan <vladislavbabayan@gmail.com>
2023-03-22 12:54:47 +0400

    feat: add new function for deleting all cart items

--- src/http/makeRequest.js
+++ src/http/makeRequest.js
@@ -1,14 +1,13 @@
 import { BASE_URL } from "../helpers";
 
-export const makeRequest = ({ url = "/", method = "GET", headers = {}, body }) => {
-  return fetch(BASE_URL + url, {
+export const makeRequest = async ({ url = "/", method = "GET", headers = {}, body }) => {
+  return await fetch(BASE_URL + url, {
     method,
     headers,
     body,
   })
     .then((res) => res.json())
     .catch((error) => {
-      console.error(error);
-      throw error;
+      throw new Error(error);
     });
 };
