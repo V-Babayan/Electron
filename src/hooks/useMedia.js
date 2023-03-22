5554c1d212b203e49922dd899a09515559c103ba
V-Babayan <vladislavbabayan@gmail.com>
2023-03-22 12:53:47 +0400

    feat: add new custom hook for working with media queries

---
+++ src/hooks/useMedia.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import { devices } from "../helpers";
+
+export const useMedia = (deviceType) => {
+  const mediaQuery = window.matchMedia(`(min-width: ${devices[deviceType].size}px)`);
+  const [media, setMedia] = useState(mediaQuery.matches);
+
+  useEffect(() => {
+    const changeHandler = () => setMedia((prev) => !prev);
+
+    mediaQuery.addEventListener("change", changeHandler);
+
+    return () => mediaQuery.removeEventListener("change", changeHandler);
+  }, []);
+
+  return media;
+};
