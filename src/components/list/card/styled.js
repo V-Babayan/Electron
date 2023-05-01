2df207eb94cd4ecf6456b14f5bf7d39ca4593242
V-Babayan <vladislavbabayan@gmail.com>
2023-05-01 16:14:19 +0400

    fix: Card component layout shift.

--- src/components/list/card/styled.js
+++ src/components/list/card/styled.js
@@ -1,18 +1,13 @@
 import styled from "styled-components";
 
 const Container = styled.li`
-  height: ${({ theme }) => theme.ratio(275, 500, "mobileS")};
-  padding: 23px 13px;
+  padding: 24px 12px;
 
   border: 1px solid #b6b6b6;
   border-radius: 20px;
 
   font-size: 16px;
   line-height: 1.6;
-
-  @media screen and (${({ theme }) => theme.media("mobile")}) {
-    height: auto;
-  }
 `;
 
 export { Container };
