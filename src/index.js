13c43e1c7b5d97dfb787a65db514a2597dcec0a2
V-Babayan <vladislavbabayan@gmail.com>
2023-05-04 17:58:43 +0400

    style: Change file structure and add new state for theme update when screen size more of 1440px

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 import { Provider } from "react-redux";
 
-import theme, { GlobalStyles } from "theme";
+import { Theme } from "styles/Theme";
 import App from "./App";
 import { store } from "./store";
 
@@ -13,10 +12,7 @@ root.render(
   <StrictMode>
     <HashRouter>
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <App />
-        </ThemeProvider>
+        <App />
       </Provider>
     </HashRouter>
   </StrictMode>
