3a82059ea69268828a77bc1df9e5b8a23ef6051f
V-Babayan <vladislavbabayan@gmail.com>
2023-05-05 18:18:14 +0400

    refactore: remove cart async get action

--- src/store/actions/asyncGetCart.js
+++
@@ -1,6 +0,0 @@
-import { getCart } from "api";
-import { setCart } from "../cartSlice";
-
-export const asyncGetCart = () => (dispatch) => {
-  getCart().then((data) => dispatch(setCart(data)));
-};
