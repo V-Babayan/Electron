4e44fe16734d4f885a6114327a201ecc0edd6d21
V-Babayan <vladislavbabayan@gmail.com>
2023-05-07 16:55:12 +0400

    fix: Fix layout shift for slider components.

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -3,8 +3,9 @@ import React, { useRef, useState } from "react";
 import { Control } from "./children";
 
 import * as Styled from "./styled";
+import { PageSkeleton } from "./children/page/skeleton";
 
-const Slider = ({ children, list }) => {
+const Slider = ({ children, list, Skeleton }) => {
   const [page, setPage] = useState(0);
   const containerRef = useRef();
 
@@ -14,23 +15,29 @@ const Slider = ({ children, list }) => {
   };
 
   return (
-    <Styled.Wrapper>
-      <Styled.Container
-        onScroll={scrollHandler}
-        ref={containerRef}>
-        {children}
-      </Styled.Container>
-      <Styled.Controls list={list ? "true" : undefined}>
-        {[...Array(children.length)].map((_, index) => (
-          <Control
-            key={index}
-            containerRef={containerRef}
-            pageNumber={index}
-            active={index === page}
-          />
-        ))}
-      </Styled.Controls>
-    </Styled.Wrapper>
+    <>
+      {!!children.length ? (
+        <Styled.Wrapper>
+          <Styled.Container
+            onScroll={scrollHandler}
+            ref={containerRef}>
+            {children}
+          </Styled.Container>
+          <Styled.Controls list={list ? "true" : undefined}>
+            {[...Array(children.length)].map((_, index) => (
+              <Control
+                key={index}
+                containerRef={containerRef}
+                pageNumber={index}
+                active={index === page}
+              />
+            ))}
+          </Styled.Controls>
+        </Styled.Wrapper>
+      ) : (
+        <Skeleton />
+      )}
+    </>
   );
 };
 
