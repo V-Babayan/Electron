71bf45991b3a7fad8efbf87a8ed54394b2e565cc
V-Babayan <vladislavbabayan@gmail.com>
2023-05-17 15:40:16 +0400

    feat: Integration new dropdown component to CategoriesMenu.

--- src/components/header/children/categories-menu/index.jsx
+++ src/components/header/children/categories-menu/index.jsx
@@ -1,25 +1,33 @@
-import React from "react";
-
 import { useSelector } from "react-redux";
 
 import { selectCategories } from "store";
 
-import { Dropdown } from "components/dropdown";
+import { Dropdown } from "components/core-ui";
 import { Category } from "./Category";
+
+import { ReactComponent as MoreIcon } from "assets/icons/more.svg";
+
+import * as Styled from "./styled";
 
 const CategoriesMenu = () => {
   const categories = useSelector(selectCategories);
 
   return (
     <Dropdown
-      defaultTitle={"Browse categories"}
-      primary>
-      {categories.map((category) => (
-        <Category
-          key={category}
-          category={category}
-        />
-      ))}
+      overlay={
+        <Styled.CategoriesList>
+          {categories.map((category) => (
+            <Category
+              key={category}
+              category={category}
+            />
+          ))}
+        </Styled.CategoriesList>
+      }>
+      <Styled.CategoriesButton>
+        Browse categories
+        <MoreIcon />
+      </Styled.CategoriesButton>
     </Dropdown>
   );
 };
