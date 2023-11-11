32bea68b459cd263dd59bfbdc95b76db0788d055
Vladislav Bababayan <vladislavbabayan@gmail.com>
2023-11-11 23:29:42 +0400

    chore: add webpack configs for import svg

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-// import { HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 // import { Provider } from "react-redux";
 
 // import { Theme } from "styles/Theme";
@@ -10,10 +10,9 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    {/* <HashRouter>
-      <Provider store={store}>
-      </Provider>
-    </HashRouter> */}
-    <App />
+    {/* <Provider store={store}></Provider> */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 );
